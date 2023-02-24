b6aafa6a5c03e4b6fcd38e27f75c52a3b64759f4
Mariana <marianagonzalezlopez01@gmail.com>
2023-02-24 11:57:30 -0600

    first commit

---
+++ OrleansURLShortener/OrleansURLShortener/Program.cs
@@ -0,0 +1,58 @@
+// <Configuration>
+using Microsoft.AspNetCore.Http.Extensions;
+using Orleans.Runtime;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseOrleans(siloBuilder =>
+{
+	siloBuilder.UseLocalhostClustering();
+	siloBuilder.AddMemoryGrainStorage("urls");
+});
+
+var app = builder.Build();
+// </Configuration>
+
+// <Endpoints>
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/shorten/{*path}",
+	async (IGrainFactory grains, HttpRequest request, string path) =>
+	{
+		// Create a unique, short ID
+		var shortenedRouteSegment = Guid.NewGuid().GetHashCode().ToString("X");
+
+		// Create and persist a grain with the shortened ID and full URL
+		var shortenerGrain = grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
+		await shortenerGrain.SetUrl(path);
+
+		// Return the shortened URL for later use
+		var resultBuilder = new UriBuilder(request.GetEncodedUrl())
+		{
+			Path = $"/go/{shortenedRouteSegment}"
+		};
+
+		return Results.Ok(resultBuilder.Uri);
+	});
+
+app.MapGet("/go/{shortenedRouteSegment}",
+	async (IGrainFactory grains, string shortenedRouteSegment) =>
+	{
+		// Retrieve the grain using the shortened ID and redirect to the original URL        
+		var shortenerGrain = grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
+		var url = await shortenerGrain.GetUrl();
+
+		return Results.Redirect(url);
+	});
+
+app.Run();
+// </Endpoints>
+
+// <GrainInterface>
+public interface IUrlShortenerGrain : IGrainWithStringKey
+{
+	Task SetUrl(string fullUrl);
+	Task<string> GetUrl();
+}
+// </GrainInterface>
+
